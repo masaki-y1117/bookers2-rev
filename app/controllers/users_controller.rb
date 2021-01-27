6aa55291c91e7c6c95372fd6e4e0211b82edaa7a
masaki-y1117 <my1117yamamoto@gmail.com>
2021-01-27 14:42:39 +0000

    [Add]rev

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,18 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    end
   end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end  
 end
