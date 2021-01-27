28d4734848e7005319777e207029ad408de5dca5
masaki-y1117 <my1117yamamoto@gmail.com>
2021-01-27 05:16:28 +0000

    [Add]rev

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:create, :index, :show, :edit, :update, :destroy]
 end
