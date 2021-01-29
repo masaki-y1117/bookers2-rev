55c23d32ffd4c397e875b2869c2c41cd4de8cbaa
masaki-y1117 <my1117yamamoto@gmail.com>
2021-01-29 11:23:29 +0000

    [Add]バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image
   
-  validates :name, uniqueness: true, length: {minimmum: 2, maxmum: 20}
-  validates :introduction, length: {maxmum: 50}
+  validates :name, uniqueness: true, length: {minimum: 2, maximum: 20 }
+  validates :introduction, length: {maximum: 50}
 end
